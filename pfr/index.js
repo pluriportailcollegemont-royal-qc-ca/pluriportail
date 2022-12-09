ad150e5e423a08e83ad569874f539f34f1b6fdca
Clément Gauthier <clement.gauthier@hotmail.com>
2022-12-09 10:32:44 -0500

    .

---
+++ pfr/index.js
@@ -0,0 +1,38 @@
+setTimeout(function() {
+    document.body.style.background = 'black';
+}, 100 );
+
+setTimeout(function() {
+    document.body.style.background = 'red';
+}, 200 );
+
+setTimeout(function() {
+    document.body.style.background = 'black';
+}, 300 );
+
+setTimeout(function() {
+    document.body.style.background = '#FF00C7';
+}, 400 );
+
+setTimeout(function() {
+    document.body.style.background = 'black';
+}, 500 );
+
+setTimeout(function() {
+    document.body.style.background = 'red';
+}, 600 );
+
+setTimeout(function() {
+    document.body.style.background = '#FF00C7';
+}, 700 );
+
+setTimeout(function() {
+    document.body.style.background = 'black';
+}, 800 );
+
+setTimeout(function() {
+    document.body.style.background = '#1D2953';
+    document.querySelector('.trolltitle').style.fontSize = "35px";
+    document.querySelector('.trolltitle').textContent = "Heureusement, ceci n'est pas une véritable attaque.";
+    document.querySelector('.hidden').classList.remove('hidden');
+}, 3000 );
